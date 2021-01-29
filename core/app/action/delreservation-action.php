bfe58113637811e4bc818a61ac8257d49e857e4d
Cristhian <cristhiansiguencia_2345@hotmail.com>
2021-01-29 12:15:12 -0500

    Cambios al proyecto

---
+++ core/app/action/delreservation-action.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* BookMedik
+* @author Cristian
+**/
+$user = ReservationData::getById($_GET["id"]);
+$user->del();
+print "<script>window.location='index.php?view=reservations';</script>";
+
+?>
